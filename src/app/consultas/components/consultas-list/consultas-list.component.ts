c1ab53c15892a6a1cc3d756124323b26acfec888
ricardosantanadev <ricardosantanadev@gmail.com>
2023-08-22 16:33:35 -0300

    update

--- src/app/consultas/components/consultas-list/consultas-list.component.ts
+++ src/app/consultas/components/consultas-list/consultas-list.component.ts
@@ -20,19 +20,17 @@ export class ConsultasListComponent {
       tap(p => console.log(p)));
   }
 
-  // metodo criado para exibir o id da na conluna consulta na tabela do angular
-  // com isso e necessario percorrer o array para extrair e retornar essa informacao
+  // metodo criado para exibir o id da consulta na conluna consulta na tabela do angular
+  // com isso e necessario percorrer o array de consultas para extrair e retornar essa informacao
   // contudo nao e a melhor forma porque o id e sobrescrito na variavel e retorna so um id, 
   // porem o paciente pode ter varias consultas ou seja mais de um id deveria serexibido
-  consultaID(c: Consulta[]): string {
+  consultaID(c: Consulta[]) {
     // dessa forma o id da consulta e exibido na tabela, porem o id e sobrescrito e mostra somente um
     let cs = '';
-    c.forEach(c => cs = c.id);
-    console.log(cs);
+    c.forEach(c => { cs = c.id, console.log(c.id) });
     return cs;
 
     // a tabela nao entede esse tipo de return por isso nao renderiza o valor na tabela
-    // necessario parassar o valor para uma variavel e retornar essa variavel conforme feito a cima
-    // return c.forEach(c => c.dataAtendimento);
+    // return c.forEach(c => c.id);
   }
 }
